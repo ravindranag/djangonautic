b49d2c1a3fe4313db1d6a562271755c37d7a4ff4
Ravindra Nag <ravindranag52@gmail.com>
2022-01-22 00:14:23 +0530

    postgress

--- djangonautic/settings.py
+++ djangonautic/settings.py
@@ -81,13 +81,23 @@ WSGI_APPLICATION = 'djangonautic.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dbuol3rdad2rca',
+        'USER': 'luxlrdxvrllmjr',
+        'PASSWORD': '0b3ffc5035350ba4b9c1931b8328cd66b1255e002cdc6d46baff24e59a6e6481',
+        'HOST': 'ec2-184-73-243-101.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
